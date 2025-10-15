50fcd7d5bd65b0c28bfbbd2c58036f408cdbaefa
tristan-spear <tspear1704@gmail.com>
2025-10-15 00:13:33 -0700

    initializing and basic functionality / styling

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-import express from express;
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.render("home.ejs"); 
+});
+
+app.get("/map", (req, res) => {
+    res.render("map.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
