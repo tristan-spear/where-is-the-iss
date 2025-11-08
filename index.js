f0ad689332a0301d26309097f6fbe7f44259eb67
tristan-spear <tspear1704@gmail.com>
2025-11-08 13:21:06 -0800

    comitting changes, api working

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
+dotenv.config(); 
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -17,17 +18,18 @@ app.get("/", (req, res) => {
 
 app.get("/map", async (req, res) => {
     try {
+      console.log(process.env.ISS_API_ID);
       const response = await axios.get("https://api.wheretheiss.at/v1/satellites/" + process.env.ISS_API_ID + "?units=miles");
       const result = {
-        latitude : response.latitude,
-        longitude : response.longitude,
-        altitude : response.altitude,
+        latitude : response.data.latitude,
+        longitude : response.data.longitude,
+        altitude : response.data.altitude,
       };
-      console.log(result);
+      
       res.render("map.ejs", { data : result });
     }
     catch(error) {
-      res.render("map.ejs", { content : JSON.stringify(error.reponse.data) });
+      res.render("map.ejs", { content : JSON.stringify(error) });
     }
 });
 
