25546157646f7a5d868da55e51d291bb15891481
tristan-spear <tspear1704@gmail.com>
2025-11-08 13:08:03 -0800

    committing progress

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";  
+
 
 const app = express();
 const port = 3000;
@@ -12,8 +15,20 @@ app.get("/", (req, res) => {
     res.render("home.ejs"); 
 });
 
-app.get("/map", (req, res) => {
-    res.render("map.ejs");
+app.get("/map", async (req, res) => {
+    try {
+      const response = await axios.get("https://api.wheretheiss.at/v1/satellites/" + process.env.ISS_API_ID + "?units=miles");
+      const result = {
+        latitude : response.latitude,
+        longitude : response.longitude,
+        altitude : response.altitude,
+      };
+      console.log(result);
+      res.render("map.ejs", { data : result });
+    }
+    catch(error) {
+      res.render("map.ejs", { content : JSON.stringify(error.reponse.data) });
+    }
 });
 
 app.listen(port, () => {
