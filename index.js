66fb4341d41a0c076a8c836984057e4ddda8fede
tristan-spear <tspear1704@gmail.com>
2025-11-09 12:24:07 -0800

    comitting progress, MVP finished

--- index.js
+++ index.js
@@ -16,17 +16,21 @@ app.get("/", (req, res) => {
     res.render("home.ejs"); 
 });
 
+function roundTo3Decimals(num) {
+  return Math.floor(num * 10000)/10000;
+}
+
 app.get("/map", async (req, res) => {
     try {
-      console.log(process.env.ISS_API_ID);
       const response = await axios.get("https://api.wheretheiss.at/v1/satellites/" + process.env.ISS_API_ID + "?units=miles");
       const result = {
-        latitude : response.data.latitude,
-        longitude : response.data.longitude,
-        altitude : response.data.altitude,
+        latitude : roundTo3Decimals(response.data.latitude),
+        longitude : roundTo3Decimals(response.data.longitude),
+        altitude : roundTo3Decimals(response.data.altitude) + " miles",
+        center : response.data.latitude + "," + response.data.longitude,
       };
       
-      res.render("map.ejs", { data : result });
+      res.render("map.ejs", { data : result , maps_key : process.env.MAPS_API_KEY });
     }
     catch(error) {
       res.render("map.ejs", { content : JSON.stringify(error) });
