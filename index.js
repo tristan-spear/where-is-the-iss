fbbb7c437400dae64e9f80faef57821d8be5b1c5
tristan-spear <tspear1704@gmail.com>
2025-12-09 17:20:48 -0800

    committing AI UI features

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.get("/", (req, res) => {
     res.render("home.ejs"); 
 });
 
+app.get("/info", (req, res) => {
+    res.render("info.ejs");
+});
+
+app.get("/game", (req, res) => {
+    res.render("game.ejs");
+});
+
 function roundTo3Decimals(num) {
   return Math.floor(num * 10000)/10000;
 }
